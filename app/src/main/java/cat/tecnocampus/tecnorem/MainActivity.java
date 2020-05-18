48793654bdb30b73fb9806943c5a59e2419beaaa
dfernandezl <dfernandezl@edu.tecnocampus.cat>
2020-05-18 13:16:47 +0200

    App print current acceleration values (x-y-z) and vibrates when detects acceleration > 3

--- app/src/main/java/cat/tecnocampus/tecnorem/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/tecnorem/MainActivity.java
@@ -1,27 +1,145 @@
 package cat.tecnocampus.tecnorem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import cat.tecnocampus.tecnorem.Fragments.MainFragment;
+public class MainActivity extends Activity implements SensorEventListener {
 
-public class MainActivity extends AppCompatActivity {
+    private float lastX, lastY, lastZ;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+
+    private TextView currentX, currentY, currentZ;
+
+    private float vibrateThreshold = 0;
+
+    public Vibrator v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeViews();
 
-        if(savedInstanceState == null){
-            MainFragment mainFragment = new MainFragment();
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.add(R.id.container, mainFragment);
-            ft.commit();
+        NumberPicker npRowSpeed = findViewById(R.id.npRowSpeed);
+
+        npRowSpeed.setMinValue(10);
+        npRowSpeed.setMaxValue(60);
+        npRowSpeed.setWrapSelectorWheel(false);
+        npRowSpeed.setOnValueChangedListener(onValueChangeListener);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+            // accelerometer OK
+
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            vibrateThreshold = 3;
         }
+        else {
+            // fail, accelerometer not available (print some message!)
+        }
+
+        //initialize vibration
+        v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public void initializeViews(){
+        currentX = (TextView) findViewById(R.id.currentX);
+        currentY = (TextView) findViewById(R.id.currentY);
+        currentZ = (TextView) findViewById(R.id.currentZ);
+    }
+
+    //onResume() register the accelerometer for listening the events
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    //onPause() unregister the accelerometer for stop listening the events
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
 
+    NumberPicker.OnValueChangeListener onValueChangeListener = new NumberPicker.OnValueChangeListener() {
+        @Override
+        public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+            TextView txtRowSpeedSelector = findViewById(R.id.txtRowSpeedSelector);
+            txtRowSpeedSelector.setText("Selected speed: "+newVal);
+        }
+    };
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // clean current values
+        displayCleanValues();
+        // display the current x,y,z accelerometer values
+        displayCurrentValues();
+
+
+        // get the change of the x,y,z values of the accelerometer
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+//        // if the change is below 2, it is just plain noise
+//        if (deltaX < 0.5)
+//            deltaX = 0;
+//        if (deltaY < 0.5)
+//            deltaY = 0;
+//        if (deltaZ < 0.5)
+//            deltaZ = 0;
+
+        // set the last know values of x,y,z
+        lastX = event.values[0];
+        lastY = event.values[1];
+        lastZ = event.values[2];
+
+        vibrate();
+    }
+
+    // if the change in the accelerometer value is big enough, then vibrate!
+    // our threshold is MaxValue/2
+    public void vibrate() {
+        if ((deltaX > vibrateThreshold) || (deltaY > vibrateThreshold) || (deltaZ > vibrateThreshold)) {
+            v.vibrate(50);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void displayCleanValues() {
+        currentX.setText("0.0");
+        currentY.setText("0.0");
+        currentZ.setText("0.0");
+    }
+
+    // display the current x,y,z accelerometer values
+    public void displayCurrentValues() {
+        currentX.setText(Float.toString(deltaX));
+        currentY.setText(Float.toString(deltaY));
+        currentZ.setText(Float.toString(deltaZ));
+    }
 }
