86a1f069c82f88842ce973ee9cae053f3fbe75ee
dfernandezl <dfernandezl@edu.tecnocampus.cat>
2020-06-12 12:21:19 +0200

    Created RowingRegister class

---
+++ app/src/main/java/cat/tecnocampus/tecnorem/Domain/RowingRegister.java
@@ -0,0 +1,11 @@
+package cat.tecnocampus.tecnorem.Domain;
+
+import java.util.Date;
+
+public class RowingRegister {
+
+    public String activityName;
+    public Date startTime;
+    public Date endTime;
+
+}
