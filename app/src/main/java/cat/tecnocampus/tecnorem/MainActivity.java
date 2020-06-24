0b581d6648351a402a1072bee5f710820f735974
dfernandezl <dfernandezl@edu.tecnocampus.cat>
2020-06-24 18:50:26 +0200

    Accelerometer refactor

--- app/src/main/java/cat/tecnocampus/tecnorem/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/tecnorem/MainActivity.java
@@ -1,16 +1,10 @@
 package cat.tecnocampus.tecnorem;
 
 import android.app.Activity;
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
-import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
@@ -19,25 +13,12 @@ import android.widget.TextView;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends Activity implements SensorEventListener {
+import cat.tecnocampus.tecnorem.Sensors.Accelerometer;
 
-    private float lastX, lastY, lastZ;
-
-    private SensorManager sensorManager;
-    private Sensor accelerometer;
-
-    private float deltaX = 0;
-    private float deltaY = 0;
-    private float deltaZ = 0;
-    private float deltaAcce = 0;
-
-    private TextView currentX, currentY, currentZ, currentAcce;
-
-    private float vibrateThreshold = 0;
-
-    public Vibrator v;
+public class MainActivity extends Activity {
 
     private static final String TAG = "MyActivity";
+
 
     private TextView chrono;
     private Button btStartPause;
@@ -52,11 +33,17 @@ public class MainActivity extends Activity implements SensorEventListener {
     private TimerTask tone;
     private int periodTime = 6000;
 
+    private Accelerometer accelerometer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initializeViews();
+
+        accelerometer = new Accelerometer(this);
+
+        //initializeViews();
+        accelerometer.initializeViews();
 
         npRowSpeed = findViewById(R.id.npRowSpeed);
 
@@ -82,34 +69,20 @@ public class MainActivity extends Activity implements SensorEventListener {
             }
         });
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
-            // accelerometer OK
-            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            vibrateThreshold = 3;
-        }
-        else {
-            // fail, accelerometer not available (print some message!)
-        }
 
-        //initialize vibration
-        v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
-    public void initializeViews(){
-        currentX = (TextView) findViewById(R.id.currentX);
-        currentY = (TextView) findViewById(R.id.currentY);
-        currentZ = (TextView) findViewById(R.id.currentZ);
-        currentAcce = (TextView) findViewById(R.id.currentAcce);
-    }
+
 
     public void stopRegister(){
         timeBuff += millisecondTime;
         handler.removeCallbacks(runnable);
-        sensorManager.unregisterListener(this);
+        //sensorManager.unregisterListener(this);
+        accelerometer.stopAccelerometer();
         timeRunning = false;
         btStartPause.setText("START");
-        displayCleanValues();
+        //displayCleanValues();
+        accelerometer.displayCleanValues();
         mp.stop();
         tone.cancel();
         mp.release();
@@ -121,7 +94,8 @@ public class MainActivity extends Activity implements SensorEventListener {
     public void startRegister(){
         startTime = SystemClock.uptimeMillis();
         handler.postDelayed(runnable,0);
-        sensorManager.registerListener(this, accelerometer,12000);
+        //sensorManager.registerListener(this, accelerometer,12000);
+        accelerometer.startAccelerometer();
         timeRunning = true;
         btStartPause.setText("STOP");
 
@@ -143,6 +117,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onResume(){
         super.onResume();
         //sensorManager.registerListener(this, accelerometer, 12000);
+        accelerometer.startAccelerometer();
     }
 
     //onPause() unregister the accelerometer for stop listening the events
@@ -161,69 +136,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
     };
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
 
-        // clean current values
-        displayCleanValues();
-        // display the current x,y,z accelerometer values
-        displayCurrentValues();
-
-
-        // get the change of the x,y,z values of the accelerometer
-        deltaX = Math.abs(lastX - event.values[0]);
-        deltaY = Math.abs(lastY - event.values[1]);
-        deltaZ = Math.abs(lastZ - event.values[2]);
-
-        deltaAcce = (float)Math.sqrt(deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ);
-
-        // if the change is below 2, it is just plain noise
-        if (deltaX < 0.5)
-            deltaX = 0;
-        if (deltaY < 0.5)
-            deltaY = 0;
-        if (deltaZ < 0.5)
-            deltaZ = 0;
-
-        if(deltaAcce < 0.5)
-            deltaAcce = 0;
-
-        // set the last know values of x,y,z
-        lastX = event.values[0];
-        lastY = event.values[1];
-        lastZ = event.values[2];
-
-        vibrate();
-        //Log.i(TAG, "Sensor Timestamp: " + event.timestamp);
-    }
-
-    // if the change in the accelerometer value is big enough, then vibrate!
-    // our threshold is MaxValue/2
-    public void vibrate() {
-        if (deltaAcce > vibrateThreshold) {
-            v.vibrate(50);
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    public void displayCleanValues() {
-        currentX.setText("0.0");
-        currentY.setText("0.0");
-        currentZ.setText("0.0");
-        currentAcce.setText("0.0");
-    }
-
-    // display the current x,y,z accelerometer values
-    public void displayCurrentValues() {
-        currentX.setText(Float.toString(deltaX));
-        currentY.setText(Float.toString(deltaY));
-        currentZ.setText(Float.toString(deltaZ));
-        currentAcce.setText(Float.toString(deltaAcce));
-    }
 
     public Runnable runnable = new Runnable() {
         @Override
---
+++ app/src/main/java/cat/tecnocampus/tecnorem/Sensors/Accelerometer.java
@@ -0,0 +1,131 @@
+package cat.tecnocampus.tecnorem.Sensors;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Vibrator;
+import android.widget.TextView;
+
+import cat.tecnocampus.tecnorem.R;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+public class Accelerometer implements SensorEventListener {
+
+    private float lastX, lastY, lastZ;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private TextView currentX, currentY, currentZ, currentAcce;
+
+    Context context;
+
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+    private float deltaAcce = 0;
+
+
+    private float vibrateThreshold = 0;
+
+    public Vibrator v;
+
+    public Accelerometer(Context context) {
+        this.context = context;
+
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+            // accelerometer OK
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            vibrateThreshold = 3;
+        }
+        else {
+            // fail, accelerometer not available (print some message!)
+        }
+
+        //initialize vibration
+        v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public void stopAccelerometer(){
+        sensorManager.unregisterListener((SensorEventListener) this);
+    }
+
+    public void startAccelerometer(){
+        sensorManager.registerListener((SensorEventListener) this, accelerometer,12000);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // clean current values
+        displayCleanValues();
+        // display the current x,y,z accelerometer values
+        displayCurrentValues();
+
+
+        // get the change of the x,y,z values of the accelerometer
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+        deltaAcce = (float)Math.sqrt(deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ);
+
+        // if the change is below 2, it is just plain noise
+        if (deltaX < 0.5)
+            deltaX = 0;
+        if (deltaY < 0.5)
+            deltaY = 0;
+        if (deltaZ < 0.5)
+            deltaZ = 0;
+
+        if(deltaAcce < 0.5)
+            deltaAcce = 0;
+
+        // set the last know values of x,y,z
+        lastX = event.values[0];
+        lastY = event.values[1];
+        lastZ = event.values[2];
+
+        vibrate();
+        //Log.i(TAG, "Sensor Timestamp: " + event.timestamp);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    // if the change in the accelerometer value is big enough, then vibrate!
+    // our threshold is MaxValue/2
+    public void vibrate() {
+        if (deltaAcce > vibrateThreshold) {
+            v.vibrate(50);
+        }
+    }
+
+    public void initializeViews(){
+        currentX = (TextView) ((Activity)context).findViewById(R.id.currentX);
+        currentY = (TextView) ((Activity)context).findViewById(R.id.currentY);
+        currentZ = (TextView) ((Activity)context).findViewById(R.id.currentZ);
+        currentAcce = (TextView) ((Activity)context).findViewById(R.id.currentAcce);
+    }
+
+    public void displayCleanValues() {
+        currentX.setText("0.0");
+        currentY.setText("0.0");
+        currentZ.setText("0.0");
+        currentAcce.setText("0.0");
+    }
+
+    // display the current x,y,z accelerometer values
+    public void displayCurrentValues() {
+        currentX.setText(Float.toString(deltaX));
+        currentY.setText(Float.toString(deltaY));
+        currentZ.setText(Float.toString(deltaZ));
+        currentAcce.setText(Float.toString(deltaAcce));
+    }
+}
